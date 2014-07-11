c821f1a060df9ed2f64fbff2a5954f4ecadeeb0c
Robert Niederreiter <office@squarewave.at>
2014-07-11 12:03:48 +0200

    attachment form wip

--- src/chronotope/browser/attachment.py
+++ src/chronotope/browser/attachment.py
@@ -1,8 +1,12 @@
 import uuid
+import pickle
+from StringIO import StringIO
 from plumber import plumber
 from node.utils import UNSET
 from yafowil.base import ExtractionError
 from pyramid.i18n import TranslationStringFactory
+from pyramid.view import view_config
+from pyramid.response import Response
 from cone.tile import (
     tile,
     Tile,
@@ -20,11 +24,52 @@ from cone.app.browser.authoring import (
     AddBehavior,
     EditBehavior,
 )
+from cone.app.browser.utils import (
+    make_url,
+    make_query,
+)
 from chronotope.model import Attachment
 from chronotope.browser import AuthoringNext
 
 
 _ = TranslationStringFactory('chronotope')
+
+
+@view_config('download', context=Attachment)
+def download(model, request):
+    # XXX: permission checks
+    a_type = model.attrs['attachment_type']
+    payload = model.attrs['payload']
+    response = Response()
+    if a_type == 'text':
+        # XXX: BeautifulSoup transform of the text
+        response.body = payload
+        response.headers['Content-Type'] = 'text/plain'
+        response.headers['Content-Disposition'] = \
+            'attachment;filename={0}'.format(model.name)
+    elif a_type == 'file':
+        payload = pickle.loads(payload)
+        response.body = payload['file']
+        response.headers['Content-Type'] = payload['mimetype']
+        response.headers['Content-Disposition'] = \
+            'attachment;filename={0}'.format(payload['filename'])
+    elif a_type == 'image':
+        payload = pickle.loads(payload)
+        scale = request.params.get('scale')
+        filename = payload['filename']
+        if scale:
+            image_data = payload['scales'][scale]
+            filename = '{0}_{1}.{2}'.format(
+                filename[:filename.rfind('.')], scale,
+                filename[filename.rfind('.') + 1:])
+        else:
+            image_data = payload['image']
+        image_data.seek(0)
+        response.body = image_data.read()
+        response.headers['Content-Type'] = payload['mimetype']
+        response.headers['Content-Disposition'] = \
+            'attachment;filename={0}'.format(filename)
+    return response
 
 
 @tile('content', 'templates/view.pt',
@@ -75,11 +120,47 @@ class AttachmentForm(object):
 
     @property
     def file_value(self):
+        a_type = self.model.attrs['attachment_type']
+        if a_type == 'file':
+            return { 'file': True }
         return UNSET
 
     @property
+    def image_vocabulary(self):
+        return [
+            ('keep', u'Keep Existing image'),
+            ('replace', u'Replace existing image'),
+        ]
+
+    @property
     def image_value(self):
+        a_type = self.model.attrs['attachment_type']
+        if a_type == 'image':
+            return { 'file': True }
         return UNSET
+
+    @property
+    def image_maxsize(self):
+        return (1600, 1600)
+
+    @property
+    def image_scales(self):
+        return {
+            'thumb': (120, 120),
+            'mini': (200, 200),
+            'preview': (400, 400),
+            'large': (1000, 1000),
+        }
+
+    @property
+    def image_src(self):
+        a_type = self.model.attrs['attachment_type']
+        if a_type == 'image':
+            query = make_query(scale='mini')
+            return make_url(self.request,
+                            node=self.model,
+                            resource='download',
+                            query=query)
 
     def attachment_field_class(self, widget, data):
         if 'type' in data:
@@ -141,9 +222,33 @@ class AttachmentForm(object):
         if a_type == 'text':
             attrs['payload'] = fetch('text')
         elif a_type == 'file':
-            print fetch('file')
+            a_file = fetch('file')
+            if a_file['action'] in ['new', 'replace']:
+                payload = dict()
+                payload['mimetype'] = a_file['mimetype']
+                payload['filename'] = a_file['filename'].filename
+                file_data = StringIO()
+                file_data.write(a_file['file'].read())
+                payload['file'] = file_data
+                attrs['payload'] = pickle.dumps(payload)
         elif a_type == 'image':
-            print fetch('image')
+            image = fetch('image')
+            if image['action'] in ['new', 'replace']:
+                payload = dict()
+                payload['mimetype'] = 'image/jpeg'
+                filename = image['original'].filename
+                filename = '{0}.jpg'.format(filename[:filename.rfind('.')])
+                payload['filename'] = filename
+                image_data = StringIO()
+                image['image'].save(image_data, 'jpeg')
+                payload['image'] = image_data
+                scales = dict()
+                for scale_name in image['scales']:
+                    scale_data = StringIO()
+                    image['scales'][scale_name].save(scale_data, 'jpeg')
+                    scales[scale_name] = scale_data
+                payload['scales'] = scales
+                attrs['payload'] = pickle.dumps(payload)
         print fetch('location')
         print fetch('facility')
         print fetch('occasion')

64efa3af672a42c6063f5187f685619d79d30a44
Robert Niederreiter <office@squarewave.at>
2014-07-28 12:15:43 +0200

    state column in container listings

---
+++ src/chronotope/browser/contents.py
@@ -0,0 +1,34 @@
+from pyramid.i18n import TranslationStringFactory
+from cone.tile import tile
+from cone.app.browser.contents import ContentsTile
+from chronotope.model import (
+    Locations,
+    Facilities,
+    Occasions,
+    Attachments,
+)
+
+
+_ = TranslationStringFactory('chronotope')
+table_template = 'cone.app.browser:templates/table.pt'
+
+
+@tile('contents', table_template, interface=Locations, permission='list')
+@tile('contents', table_template, interface=Facilities, permission='list')
+@tile('contents', table_template, interface=Occasions, permission='list')
+@tile('contents', table_template, interface=Attachments, permission='list')
+class ChronotopeContentsTile(ContentsTile):
+    col_defs = ContentsTile.col_defs + [{
+        'id': 'state',
+        'title': _('state', 'State'),
+        'sort_key': 'state',
+        'sort_title': _('sort_on_state', 'Sort on state'),
+        'content': 'string'
+    }]
+    sort_keys = ContentsTile.sort_keys.copy()
+    sort_keys['state'] = lambda x: x.attrs['state']
+
+    def row_data(self, node):
+        row_data = super(ChronotopeContentsTile, self).row_data(node)
+        row_data['state'] = node.attrs['state']
+        return row_data
